379eadcb02a399d5ebf5a92d8df78b91ef24f55c
peshane <social@peshane.net>
2019-11-08 13:36:38 +0100

    revert name change and fix name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pylastic-peshane",
+    name="pylastic-jelastic-peshane",
     version="0.0.1",
     author="Laurent Fuentes",
     author_email="social@peshane.net",
