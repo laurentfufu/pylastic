1e7e82c6f115945a09255abf21d80b62d58de75e
peshane <social@peshane.net>
2019-11-09 16:32:06 +0100

    hop

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pylastic-peshane",
-    version="0.0.1",
+    version="0.0.2",
     author="Laurent Fuentes",
     author_email="social@peshane.net",
     description="Interact with Jelastic API",
